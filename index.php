0445a9e4609843c2f4d8e1b96741e57338d61f34
antoineegret <a.egret@laposte.net>
2019-12-05 14:34:34 +0100

    exercie 3 & 8

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>exo1</title>
+</head>
+<body>
+    <h2>exercice 3</h2>
+    <?php 
+    $km = "125";
+    $km1 = "1";
+    $km2 = "3";
+    echo $km; 
+    ?>
+    <br>
+    <?php echo $km1;?>
+    <br>
+    <?php echo $km2;?>
+    <br>
+    <h2>exercice 8</h2>
+    <br>
+    <?php $x = 3;
+          $y = 4;
+          $z = 5;
+          $v = 20;
+          $w = 45;  
+    echo $x + $y; ?> <br>
+    <?php
+    echo $z * $v; ?> <br>
+    <?php
+    echo $w / $z;
+    ?>
+</body>
+</html>
